57884db5f75c6225a74f707c78667043a161b117
probir270 <53487325+probir270@users.noreply.github.com>
2020-04-14 17:45:32 +0530

    Movements

---
+++ Polyream game jam/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [Header("Settings")]
+    public float moveSpeed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateMovements();
+    }
+    void CalculateMovements()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon)
+        {
+            horizontalMovement = horizontalMovement * Time.deltaTime * moveSpeed;
+            horizontalMovement += transform.position.x;
+            transform.position = new Vector2(horizontalMovement, transform.position.y);
+        }
+
+    }
+}
